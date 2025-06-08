791d720aef3803e2b2d1294c6b8bd0973cd77edb
Torrivor <breseker2000@gmail.com>
2025-06-08 17:40:29 +0200

    working nmp run dev

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+ 
+app.get('/', (req, res) => {
+  res.send('Witaj w Express!');
+});
+ 
+app.listen(PORT, () => {
+  console.log(`Serwer działa na http://localhost:${PORT}`);
+});
